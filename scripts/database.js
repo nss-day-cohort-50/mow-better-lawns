c785ffdf7bb8091030f3508c159d7187cfc1769d
Steve Brownlee <chortlehoort@gmail.com>
2021-07-28 13:20:05 -0500

    Working

--- scripts/database.js
+++ scripts/database.js
@@ -1,51 +1,57 @@
 const database = {
     kids: [
         {
-            id: 1, 
+            id: 1,
             name: "Justin",
             age: 10
         },
         {
-            id: 2, 
+            id: 2,
             name: "Eric",
             age: 42
         },
         {
-            id: 3, 
+            id: 3,
             name: "Erin",
             age: 13
         },
         {
-            id: 4, 
+            id: 4,
             name: "Myriam",
             age: 14
         }
     ],
-    homes: [ 
+    homes: [
         {
-        id: 1,
-        address: "333 Scissorhands lane",
-        size: .25,
-        serviceId: 4
-    },
+            id: 1,
+            address: "333 Scissorhands lane",
+            size: .25,
+            serviceId: 4
+        },
         {
-        id: 2,
-        address: "47 Mars Ave",
-        size: 1.25,
-        serviceId: 2
-    },
+            id: 2,
+            address: "47 Mars Ave",
+            size: 1.25,
+            serviceId: 2
+        },
         {
-        id: 3,
-        address: "1600 Pennsylvania Ave",
-        size: 3,
-        serviceId: 1
-    },
+            id: 3,
+            address: "1600 Pennsylvania Ave",
+            size: 3,
+            serviceId: 1
+        },
         {
-        id: 4,
-        address: "10 Cloverfield Lane",
-        size: 1,
-        serviceId: 3
-    }
+            id: 4,
+            address: "10 Cloverfield Lane",
+            size: 1,
+            serviceId: 3
+        },
+        {
+            id: 5,
+            address: "5592 Kuvalis Unions",
+            size: 8,
+            serviceId: 4
+        }
     ],
     services: [
         {
@@ -98,14 +104,14 @@ const database = {
 }
 
 export const getKids = () => {
-    return database.kids.map(kid => ({...kid}))
+    return database.kids.map(kid => ({ ...kid }))
 }
 export const getHomes = () => {
-    return database.homes.map(home => ({...home}))
+    return database.homes.map(home => ({ ...home }))
 }
 export const getServices = () => {
-    return database.services.map(service => ({...service}))
+    return database.services.map(service => ({ ...service }))
 }
 export const getKidHomeAssignments = () => {
-    return database.kidHomeAssignments.map(kidhome => ({...kidhome}))
+    return database.kidHomeAssignments.map(kidhome => ({ ...kidhome }))
 }
--- scripts/kids.js
+++ scripts/kids.js
@@ -12,30 +12,26 @@ const homes = getHomes();
 const services = getServices();
 const kidHomeAssignments = getKidHomeAssignments();
 
+
 const findAssignmentsForKid = (kidObject, kidHomeAssignmentsArray) => {
   let kidHomes = [];
-    for (const kidHomeAssignmentObject of kidHomeAssignmentsArray) {
-      if (kidObject.id === kidHomeAssignmentObject.kidId) {
-        kidHomes.push(kidHomeAssignmentObject)
-      }
+  for (const kidHomeAssignmentObject of kidHomeAssignmentsArray) {
+    if (kidObject.id === kidHomeAssignmentObject.kidId) {
+      kidHomes.push(kidHomeAssignmentObject)
     }
+  }
   return kidHomes;
 };
 
-const findServices = (servicesArray, homesArray) => {
-  let serviceRendered = null;
-  for (const serviceObject of servicesArray) {
-    for (const homeObject of homesArray) {
-      if (serviceObject.id === homeObject.serviceId) {
-        serviceRendered = serviceObject;
-      }
+const findHomeForAssignment = (assignmentHomeId, arrayOfHomes) => {
+  let theHomeIFound = null
+
+  for (const homeObject of arrayOfHomes) {
+    if (homeObject.id === assignmentHomeId) {
+      theHomeIFound = homeObject
     }
   }
-  return serviceRendered;
-};
-
-const findHome = () => {
-    //do stuff here 
+  return theHomeIFound;
 }
 
 export const Kids = () => {
@@ -43,13 +39,24 @@ export const Kids = () => {
   html = "<ul>";
 
   for (const kid of kids) {
-    const assignments = findAssignmentsForKid(kid, kidHomeAssignments);
-    const foundService = findServices(services, homes);
-    for (const assignment of assignments) {
+    const filteredAssignmentsForThisKid = findAssignmentsForKid(kid, kidHomeAssignments)
 
+    for (const assignment of filteredAssignmentsForThisKid) {
+      const matchingHome = findHomeForAssignment(assignment.homeId, homes)
 
+      const homeService = services.find(
+        (serviceObject) => {
+          return serviceObject.id === matchingHome.serviceId
+        }
+      )
+
+      html += `<li id="kid--${kid.id}">${kid.name}
+      is assigned to ${matchingHome.address} for the
+      ${homeService.serviceType} service
+      </li>`;
     }
-    html += `<li id="kid--${kid.id}">${kid.name} ${foundService.serviceType} at ${.address}</li>`;
+
+
   }
   html += "</ul>";
   return html;
